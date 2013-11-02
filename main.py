fc25c2ed347840782ba879418973b9f31e643693
Sebastian <sb@flexpage.net>
2013-11-02 08:55:32 +0100

    Added MostPeripheralsStrategy

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import random
 from termcolor import colored
 
 from playground import PlayGround, Position, FieldColorPalette
-from solver import Area, Solver, RandomStrategy, MostPeripheralsStrategy
+from solver import Area, Solver, RandomStrategy, MostPeripheralsStrategy, \
+                   MostUnfloodedColorStrategy
 
 def main(args):
     print "Flood fill"
@@ -14,18 +15,23 @@ def main(args):
     fp = FieldColorPalette(num_colors=6)
     pg = PlayGround(12, 12, fp)
     pg.fill_random()
+    pg.plot()
 
     p = pg.copy()
-    p.plot()
-
     solver1 = Solver(p, RandomStrategy(p))
     solver1.solve()
 
-    solver2 = Solver(pg, MostPeripheralsStrategy(pg))
+    p = pg.copy()
+    solver2 = Solver(p, MostPeripheralsStrategy(p))
     solver2.solve()
+
+    p = pg.copy()
+    solver3 = Solver(p, MostUnfloodedColorStrategy(p))
+    solver3.solve()
 
     solver1.print_solution()
     solver2.print_solution()
+    solver3.print_solution()
 
 
 if __name__ == '__main__':
