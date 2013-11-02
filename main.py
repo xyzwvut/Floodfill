f542baaea3d322f2aaa2db578b1a3865c23b3322
Sebastian <sb@flexpage.net>
2013-11-02 22:57:10 +0100

    Test multiple random runs and current best

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ def parse_cmdline(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', '--verbosity', action='count', default=0,
                         help='increase output verbosity')
+    parser.add_argument('--iterations', type=int, default=50,
+                        help='number of iterations')
 
     args = parser.parse_args(argv)
     config['args'] = args
@@ -34,15 +36,33 @@ def main(argv):
 
     p = pg.copy()
     solver1 = Solver(p, RandomStrategy(p))
-    print solver1.solve()
+    sol1 = solver1.solve()
 
     p = pg.copy()
     solver2 = Solver(p, MostPeripheralsStrategy(p))
-    print solver2.solve()
+    sol2 = solver2.solve()
 
     p = pg.copy()
     solver3 = Solver(p, MostUnfloodedColorStrategy(p))
-    print solver3.solve()
+    sol3 = solver3.solve()
+
+    print sol1
+    print sol2
+    print sol3
+
+    if 'iterations' in config['args']:
+        steps = config['args'].iterations
+
+    print "Giving Random strategy %s tries" % steps
+
+    bs = sol1
+    for i in range(steps):
+        p = pg.copy()
+        solver1 = Solver(p, RandomStrategy(p))
+        sol = solver1.solve()
+        if sol.better_than(bs):
+            bs = sol
+            print bs
 
 
 if __name__ == '__main__':
