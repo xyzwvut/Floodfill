f0b43d13085e28536fa8075a3b62978b610d93e0
Sebastian <sb@flexpage.net>
2013-11-08 19:13:27 +0100

    Added newline

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from playground import PlayGround, Position, FieldColorPalette
 from solver import Area, Solver, RandomStrategy, MostPeripheralsStrategy, \
                    MostUnfloodedColorStrategy
 
+
 def parse_cmdline(argv):
     """Parse applications commandline and feed into global config"""
     parser = argparse.ArgumentParser()
